36f5594f3a599e7cb6156a6b24bfaeef8b81774a
Joe Sweeny <joesweeny84@hotmail.com>
2020-02-06 22:25:46 +0000

    Add user controller comments route

--- tests/Http/Controllers/UserControllerTest.php
+++ tests/Http/Controllers/UserControllerTest.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Boundary\Comment\CommentService;
 use App\Boundary\User\UserService;
 use PHPUnit\Framework\TestCase;
 use Prophecy\Prophecy\ObjectProphecy;
+use Ramsey\Uuid\Exception\InvalidUuidStringException;
 
 class UserControllerTest extends TestCase
 {
@@ -13,6 +15,10 @@ class UserControllerTest extends TestCase
      */
     private $userService;
     /**
+     * @var CommentService|ObjectProphecy
+     */
+    private $commentService;
+    /**
      * @var UserController
      */
     private $controller;
@@ -20,10 +26,14 @@ class UserControllerTest extends TestCase
     public function setUp(): void
     {
         $this->userService = $this->prophesize(UserService::class);
-        $this->controller = new UserController($this->userService->reveal());
+        $this->commentService = $this->prophesize(CommentService::class);
+        $this->controller = new UserController(
+            $this->userService->reveal(),
+            $this->commentService->reveal()
+        );
     }
 
-    public function test_get_returns_a_json_response_containing_user_scalar_user_data()
+    public function test_list_returns_a_json_response_containing_scalar_user_data()
     {
         $users = [
             (object) [
@@ -38,14 +48,59 @@ class UserControllerTest extends TestCase
 
         $this->userService->getAllUsers()->willReturn($users);
 
-        $response = $this->controller->get();
+        $response = $this->controller->list();
         $body = json_decode($response->getContent());
-        
+
         $expected = (object) [
             'users' => $users,
         ];
 
         $this->assertEquals(200, $response->getStatusCode());
+        $this->assertEquals($expected, $body);
+    }
+
+    public function test_comments_returns_a_json_response_containing_scalar_comment_data()
+    {
+        $comments = [
+            (object) [
+                'id' => '80b5894b-25c2-4a0c-926b-0f476e92f788',
+                'user_id' => '1da15c2e-f2fa-45ca-9c71-277428258931',
+                'game_id' => 'e09313c7-b55f-471f-81c2-47d879ad8dfd',
+                'text' =>  'Was hoping for something more',
+            ],
+            (object) [
+                'id' => '48f76f41-79f7-4279-bb47-2b61df42e1b0',
+                'user_id' => '1da15c2e-f2fa-45ca-9c71-277428258931',
+                'game_id' => 'e09313c7-b55f-471f-81c2-47d879ad8dfd',
+                'text' =>  'Pretty awesome',
+            ],
+        ];
+
+        $this->commentService->getByUserId('1da15c2e-f2fa-45ca-9c71-277428258931')->willReturn($comments);
+
+        $response = $this->controller->comments('1da15c2e-f2fa-45ca-9c71-277428258931');
+        $body = json_decode($response->getContent());
+
+        $expected = (object) [
+            'comments' => $comments,
+        ];
+
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertEquals($expected, $body);
+    }
+
+    public function test_comments_returns_422_response_if_id_passed_is_an_invalid_uuid_string()
+    {
+        $this->commentService->getByUserId('1')->willThrow(new InvalidUuidStringException('Invalid uuid'));
+
+        $response = $this->controller->comments('1');
+        $body = json_decode($response->getContent());
+
+        $expected = (object) [
+            'error' => 'Invalid uuid',
+        ];
+
+        $this->assertEquals(422, $response->getStatusCode());
         $this->assertEquals($expected, $body);
     }
 }
